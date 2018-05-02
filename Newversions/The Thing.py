472d3582f5c74a2e191a3a85567fc429ca0ec35a
Owen Cochell <owencochell@gmail.com>
2018-05-02 16:54:51 -0400

    Update The Thing.py

--- Newversions/The Thing.py
+++ Newversions/The Thing.py
@@ -4,35 +4,40 @@ import animate.py as ani
 #Code for passwords here
 authe=False
 interm=True
+on=True
+norm=True
 temp=20
 tec='Windows'
 pio='Mcrege'
 wep='Gary'
 bio='Blair'
 
+while on==True:
 #Boot up procedure
 boot()
-#Usernames and passwords
-while authe==False:
-    print("Welcome to Outpost Interface Manager login(ver. 1.73.48)")
-    print("Please login:")
-    usern=input("Username:")
-    passw=input("Password:")
-    auth(authe, tec, pio, wep, bio, tecp, piop, wepp, biop)
-    if authe==False:
-        print("Error: \n #Incorect username/Password!")
-    if authe==True:
-        break
-#Login procedure
-ani.login()
-#Code for Terminal welcome screen
-startt(usern)
-while interm==True:
-    inp=input("C:\Users\%s>>>")(usern)
-    if inp=='help':
-        help(usern)
-    if inp=='logout':
-        print("Starting 'LOGOUT.SH(C:\SYS\ACCOUNT)'...")
-        logout()
-
+#Check to see which version to boot
+    if norm==True:    
+    #Usernames and passwords
+    while authe==False:
+        print("Welcome to Outpost Interface Manager login(ver. 1.73.48)")
+        print("Please login:")
+        usern=input("Username:")
+        passw=input("Password:")
+        auth(authe, tec, pio, wep, bio, tecp, piop, wepp, biop)
+        if authe==False:
+            print("Error: \n #Incorect username/Password!")
+        if authe==True:
+            break
+    #Login procedure
+    ani.login()
+    #Code for Terminal welcome screen
+    startt(usern)
+    while interm==True:
+        inp=input("C:\Users\%s>>>")(usern)
+        if inp=='help':
+            help(usern)
+        if inp=='logout':
+            print("Starting 'LOGOUT.SH(C:\SYS\ACCOUNT)'...")
+            logout()
+    
     
