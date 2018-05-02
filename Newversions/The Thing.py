1dfc80b73e7c85f7e76c65de8f8b75e2fbba5b28
Owen Cochell <owencochell@gmail.com>
2018-05-02 16:56:49 -0400

    Update The Thing.py

--- Newversions/The Thing.py
+++ Newversions/The Thing.py
@@ -13,21 +13,21 @@ wep='Gary'
 bio='Blair'
 
 while on==True:
-#Boot up procedure
-boot()
-#Check to see which version to boot
+    #Boot up procedure
+    boot()
+    #Check to see which version to boot
     if norm==True:    
-    #Usernames and passwords
-    while authe==False:
-        print("Welcome to Outpost Interface Manager login(ver. 1.73.48)")
-        print("Please login:")
-        usern=input("Username:")
-        passw=input("Password:")
-        auth(authe, tec, pio, wep, bio, tecp, piop, wepp, biop)
-        if authe==False:
-            print("Error: \n #Incorect username/Password!")
-        if authe==True:
-            break
+        #Usernames and passwords
+        while authe==False:
+            print("Welcome to Outpost Interface Manager login(ver. 1.73.48)")
+            print("Please login:")
+            usern=input("Username:")
+            passw=input("Password:")
+            auth(authe, tec, pio, wep, bio, tecp, piop, wepp, biop)
+            if authe==False:
+                print("Error: \n #Incorect username/Password!")
+            if authe==True:
+                break
     #Login procedure
     login()
     #Code for Terminal welcome screen
