754ac300f509490ae0271907dbeb196256e535bf
Owen Cochell <owencochell@gmail.com>
2018-05-22 17:22:13 -0400

    Update The Thing.py

--- Newversions/The Thing.py
+++ Newversions/The Thing.py
@@ -1,5 +1,6 @@
 #This file is where all code will be executed
 import time
+import os
 from animate import *
 #Code for passwords here
 suc='#Success!'
@@ -7,6 +8,8 @@ authe=False
 interm=False
 on=True
 norm=True
+lockcon=False
+lockpass='test123'
 temp=20
 tec='Windows'
 pio='Mcrege'
@@ -15,11 +18,27 @@ bio='Blair'
 
 while on==True:
     #Boot up procedure
+    clear()
     boot(suc)
     #Check to see which version to boot
+    if norm==False:
+        if lockcon==True:
+            clear()
+            lockout()
+            lockinp=input("Please enter password or security overide code:")
+            if lockinp==lockpass:
+                p
+            if lockinp==wepp:
+                p
+            else:
+                clear()
+                lerror(lockinp)
+                continue
+                
     if norm==True:    
         #Usernames and passwords
         while authe==False:
+            clear()
             print("Welcome to Outpost Interface Manager login(ver. 1.73.48)")
             print("Please login:")
             usern=input("Username:")
@@ -27,13 +46,16 @@ while on==True:
             auth(authe, tec, pio, wep, bio, tecp, piop, wepp, biop, usern, passw)
             authe=auth(authe, tec, pio, wep, bio, tecp, piop, wepp, biop, usern, passw)
             if authe==False:
+                clear()
                 print("Error: \n#Incorect username/Password!")
             if authe==True:
                 break
     #Login procedure
+    clear()
     login(suc)
     interm=True
     #Code for Terminal welcome screen
+    clear()
     startt(usern)
     while interm==True:
         inp=input("C:\Users\%s>>>" % usern)
@@ -44,10 +66,12 @@ while on==True:
             time.sleep(1)
             interm=False
             authe=False
+            clear()
             logout(suc)
         if inp=='file':
             print("Starting 'FILE.SH(C:\SYS\FILEMAN)'...")
             time.sleep(1)
+            clear()
             files(suc)
             file()
             print("No files yet at this time")
@@ -59,11 +83,34 @@ while on==True:
         if inp=='info':
             print("This feature is not yet implemented.")
         if inp=='clear':
-            print("This feature is not yet implemented.")
+            clear()
         if inp=='lockdown':
-            print("This feature is not yet implemented.")
+            if usern==wep:
+                lockinfo()
+                continue
+            else:
+                terror(inp)
+                continue
         if inp=='lockdown_int':
-            print("This feature is not yet implemented.")
+            print("This feature is currently in development")
+            if usern==wep:
+                lockcon()
+                lockpass=lockcon(lockpass, lockconf)
+                lockconf=lockcon(lockpass, lockconf)
+                if lockconf==True:
+                    print("Starting 'LOCKDOWN.SH(C:\SYS\SECURITY)'...")
+                    time.sleep(1)
+                    clear()
+                    lockint(suc)
+                    norm=False
+                    interm=False
+                    auth=False
+                    continue
+                if lockconf==False:
+                    continue
+            else:
+                terror(inp)
+                continue
         if inp=='lockdown_dis':
             print("This feature is not yet implemented.")
         if inp=='fridge_config':
